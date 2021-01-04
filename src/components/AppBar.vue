e0a784cd6fcb2adac21649df0f949aad255164a3
Ruinous <sat2@vip.qq.com>
2021-01-04 22:25:14 +0800

    添加AppBar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,15 +1,13 @@
 <template>
-<v-container>
-     <v-app-bar app  flat color="#B9F6CA">
+         <v-app-bar app  flat color="#B9F6CA">
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
         <v-app-bar-title>吉林大学树洞</v-app-bar-title>
-     </v-app-bar>
-</v-container>
+      </v-app-bar>
 </template>
 
 <script>
 export default {
-  name: 'Index',
+  name: 'AppBar',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
