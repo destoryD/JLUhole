44c52de0296f0c1ad57123afe66a7752169efa78
Ruinous <sat2@vip.qq.com>
2021-01-07 21:28:57 +0800

    优化appbar逻辑

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,9 +1,13 @@
 <template>
-    <v-app-bar app flat hide-on-scroll>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+<v-card>
+    <v-app-bar app flat hide-on-scroll >
+              <v-btn icon>
+        <v-icon large >mdi-home-variant </v-icon>
+      </v-btn>
         <v-row       align="center"
-      justify="center" style="padding-left:1rem">
+      justify="center" style="padding:1rem">
       <v-text-field
+      v-model="searchtext"
       id="searchbox"
         hide-details
         solo
@@ -12,15 +16,25 @@
         single-line
         class="rounded-pill"
         justify="center"
-        style=""
+        style="text-indext:1rem"
         placeholder="吉林大学树洞正式开通了"
       ></v-text-field>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
         </v-row>
+              <v-btn icon>
+        <v-icon large color="#64B5F6">mdi-plus-circle</v-icon>
+      </v-btn>
+      <template v-slot:extension>
+        <v-tabs v-model="tabselected"
+         align-with-titles>
+          <v-tabs-slider color="transparent"></v-tabs-slider>
+          <v-tab v-for="tabitem in tabitems" :key="tabitem.label" v-show="tabitem.active" @click="tabselect">{{tabitem.label}}</v-tab>
+        </v-tabs>
+                          <v-btn icon>
+        <v-icon color="black" >mdi-magnify </v-icon>
+      </v-btn>
+      </template>
     </v-app-bar>
+</v-card>
 </template>
 
 <script>
@@ -28,8 +42,27 @@ export default {
   name: 'AppBar',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      text: ''
+      searchtext: '',
+      tabselected: 1,
+      tabitems: [
+        {
+          label: '热榜',
+          active: true
+        },
+        {
+          label: '推荐',
+          active: true
+        },
+        {
+          label: '最新',
+          active: true
+        }
+      ]
+    }
+  },
+  methods: {
+    tabselect () {
+      // console.log(this.tabselected)
     }
   }
 }
