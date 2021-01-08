e529e55fe1551225a0aaf37a96881261fe30c365
Ruinous <sat2@vip.qq.com>
2021-01-08 15:28:28 +0800

    增加下拉刷新和上拉加载功能

--- src/components/IndexContent.vue
+++ src/components/IndexContent.vue
@@ -1,10 +1,11 @@
 <template>
 <div id="content">
-  <v-card outlined v-for="card in carddata" :key="card.Title">
+    <scroller :on-refresh="onRefresh" :on-infinite="OnInfinite" ref="content_scroller">
+  <v-card outlined v-for="(card,index) in carddata" :key="card.Title + index">
     <v-container fluid>
     <v-card-title class="font-weight-bold text-subtitle-1 text-left" style="padding-left:0;padding-top:0;">{{card.Title}}</v-card-title>
     <v-card-text>
-        <v-row class="text-left " style="height:2.5rem;text-overflow:ellipsis;overflow:hidden">{{card.Content}}</v-row>
+        <v-row class="text-left " style="display: -webkit-box;-webkit-line-clamp: 3;-webkit-box-orient: vertical;text-overflow:ellipsis;overflow:hidden">{{card.Content}}</v-row>
     </v-card-text>
     <v-card-text style="padding-bottom:0.5rem">
         <v-row class="text-left" style="color:rgba(0,0,0,.6)">{{card.hot}}热度 · {{card.posts}}评论<v-spacer></v-spacer>{{card.LastUpdateTimestamp}}</v-row>
@@ -12,6 +13,7 @@
     </v-container>
     <div class="custom-divider" style="background-color:rgba(0,0,0,.12);height:0.25rem;width:100%"></div>
   </v-card>
+    </scroller>
 </div>
 </template>
 <script>
@@ -25,6 +27,20 @@ export default {
   },
   directives: {
     swiper: directive
+  },
+  methods: {
+    onRefresh (done) {
+      console.log('refresh')
+      setTimeout(() => {
+        done(true)
+      }, 1000)
+    },
+    OnInfinite (done) {
+      console.log('infinite')
+      setTimeout(() => {
+        done(true)
+      }, 1000)
+    }
   },
   data () {
     return {
@@ -48,9 +64,25 @@ export default {
         },
         {
           Title: '如果用AMD的CPU配上N卡会有什么效果？',
+          Content: '我这是测试一下内容，小新测试赛我这是测试一下一下内容，小新测试赛我这是测试一',
+          PostTimestamp: 1610026871,
+          LastUpdateTimestamp: new Date(1611026871000).toLocaleDateString() + ' ' + new Date(1601026871000).toLocaleTimeString(),
+          hot: 0,
+          posts: 1
+        },
+        {
+          Title: '如果用AMD的CPU配上N卡会有什么效果？',
           Content: '我这是测试一下内容，小新测试赛我这是测试一下内容，小新测试赛我这是测试一下内容，小新测试赛我这是测试一下内容，小新测试赛我这是测试一下内容，小新测试赛我这是测试一下内容，小新测试赛',
           PostTimestamp: 1610026871,
           LastUpdateTimestamp: new Date(1610026871000).toLocaleDateString() + ' ' + new Date(1600026871000).toLocaleTimeString(),
+          hot: 2000,
+          posts: 100
+        },
+        {
+          Title: '如果用AMD的CPU配上N卡会有什么效果？',
+          Content: '我这是测试一下内容，小新测试赛我这是测试一下一下内容，小新测试赛我这是测试一',
+          PostTimestamp: 1610026871,
+          LastUpdateTimestamp: new Date(1611026871000).toLocaleDateString() + ' ' + new Date(1601026871000).toLocaleTimeString(),
           hot: 0,
           posts: 1
         }
--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import vuetify from '@/plugins/vuetify'
+import vuescroller from 'vue-scroller'
 Vue.config.productionTip = false
+Vue.use(vuescroller)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
